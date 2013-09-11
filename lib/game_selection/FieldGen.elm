1a79b69286d8f18235dac854ec48ae6db978f52a
ZacharyKamerling <ZacharyKamerling@gmail.com>
2013-09-11 09:09:00 -0700

    Chatbox
    Added a chatbox beneath the score graph.

--- lib/game_selection/FieldGen.elm
+++ lib/game_selection/FieldGen.elm
@@ -1,7 +1,8 @@
-module FieldGen (fieldGen) where
+module FieldGen (fieldGen,chatbox) where
 
 import Dict as D
 import Graphics.Input as I
+import Keyboard as K
 
 -- Create an infinite list of fields.
 
@@ -11,9 +12,9 @@ type FieldGen =
     (I.FieldState -> (Int,I.FieldState)) -> String -> I.FieldState -> Element
   }
 
+-- Creates a signal containing a vertical list of fields, and the strings contained in the fields.
 fieldGen : String -> Signal (Element,[String])
-fieldGen txt = 
-  let fg = I.fields (-1,I.emptyFieldState) in
+fieldGen txt = let fg = I.fields (-1,I.emptyFieldState) in
   lift (\(n,xs) ->
     (flow down <| fg.field ((,) -1) txt I.emptyFieldState :: 
       map (\(i,st) -> fg.field ((,) i) txt st) xs, map (.string . snd) xs)) <| 
@@ -30,12 +31,31 @@ fieldGen txt =
            then (n, remove i xs)
            -- Update field
            else (n, insert i st xs)) 
-  (0, []) fg.events
+    (0, []) 
+    fg.events
 
+insert : Int -> a -> [(Int,a)] -> [(Int,a)]
 insert n v xs = case xs of
   (n',v')::xs -> if n' == n then (n,v)::xs else (n',v')::insert n v xs
   _           -> [(n,v)]
 
+remove : Int -> [(Int,a)] -> [(Int,a)]
 remove n xs = case xs of
   (n',v)::xs -> if n' == n then xs else (n',v)::remove n xs
   _          -> []
+
+chatbox : (Signal Element, Signal String)
+chatbox = 
+  let cbGen = I.fields I.emptyFieldState
+      fieldState = foldp 
+        (\(pressed,st') _ -> 
+          if pressed && st'.string /= "" 
+          then (cbGen.field id "Say something" I.emptyFieldState, st'.string)
+          else (cbGen.field id "Say something" st'              , "")
+        )
+        (cbGen.field id "Say something" I.emptyFieldState, "")
+        ((,) <~ K.enter ~ cbGen.events) 
+  in
+    ( fst <~ fieldState
+    , snd <~ fieldState
+    )
--- lib/game_selection/GameSelection.elm
+++ lib/game_selection/GameSelection.elm
@@ -2,46 +2,50 @@ module GameSelection where
 
 import Graphics.Input as Input
 import Scenarios (scenario1,scenario2,scenario3)
-import ScoreGraph (scoreGraph)
-import FieldGen (fieldGen)
+import FieldGen (fieldGen,chatbox)
+import Mouse
+import Graph
+import Window
 
 type Score = Float
 type Team = {name : String, teamColor : Color, scores : [(Time,Score)], players : [String]}
 type Game = {teams : [Team]}
+
+-- Scores generated from mouse x position
+team1Scores : Signal (Color,[(Time,Float)])
+team1Scores = (,) <~ constant (rgb 255 100 0) ~ foldp (\(t,x) xs -> take 5000 <| (t,toFloat x) :: xs) []
+  (timestamp <| sampleOn (fps 10) Mouse.x)
+
+-- Scores generated from mouse y position
+team2Scores : Signal (Color,[(Time,Float)])
+team2Scores = (,) <~ constant blue ~ foldp (\(t,x) xs -> take 5000 <| (t,toFloat x) :: xs) []
+  (timestamp <| sampleOn (fps 10) Mouse.y)
+
+-- Scores generated from sin wave
+team3Scores : Signal (Color,[(Time,Float)])
+team3Scores = (,) <~ constant green ~ foldp (\t xs -> take 5000 <| (t, snd (head xs) + sin t * 6 + 0.1) :: xs) [(0,50)] (every <| second / 10)
+
+-- Scores generated from cos wave
+team4Scores : Signal (Color,[(Time,Float)])
+team4Scores = (,) <~ constant purple ~ foldp (\t xs -> take 5000 <| (t, snd (head xs) + cos t * 6 + 0.1) :: xs) [(0,50)] (every <| second / 10)
+
+scoreGraph = (\xs wx wy -> Graph.timeline wx (wy `div` 2) (toFloat wx/(minute * 5)) (1/2) xs) <~ 
+  combine [team1Scores, team2Scores, team3Scores, team4Scores] ~ Window.width ~ Window.height
 
 welcome = 
   [markdown|
     Welcome to Edurange
     ===================
 
-    Select the scenarios you want to play.
+    Select the scenarios you want to play. 
+    Each scenario you select will increase 
+    the number of VM's that EDURange starts.
   |]
 
+-- (Element, Signal Bool)
 (startGame, gameStarted) =
   let (btn,start) = Input.button "Start Game" in
   (btn, foldp (\_ go -> not go) False start)
-
--- Setup the UI of the entire page
-draw : Element -- Start Game Button
-    -> [Element] -- Scenarios
-    -> Element   -- Score Graph
-    -> (Element,[String]) -- (Team 1 Fields, Team 1 Names)
-    -> (Element,[String]) -- (Team 2 Fields, Team 2 Names)
-    -> Bool -- Game Started
-    ->  Element
-draw startGame scenarios scoreGraph team1 team2 gameStarted = 
-  if   gameStarted
-  then scoreGraph
-  else spacer 20 1 `beside` flow down
-      [ welcome
-      , flow down scenarios
-      , spacer 1 40
-      , fst team1
-      , spacer 1 40
-      , fst team2
-      , spacer 1 40
-      , startGame
-      ]
 
 (team1NameE,team1NameS) = Input.field "Team 1 Name"
 (team2NameE,team2NameS) = Input.field "Team 2 Name"
@@ -52,9 +56,52 @@ team1Players = fieldGen "Team 1 Player Name"
 team2Players : Signal (Element,[String])
 team2Players = fieldGen "Team 2 Player Name"
 
-main = draw startGame <~ 
+(msgBox,message) = chatbox
+
+messages = foldp (::) [] message
+
+-- Setup the UI of the entire page
+draw : Bool -- Game Started
+    -> Element -- Start Game Button
+    -> [Element] -- Scenarios
+    -> (Element,[String]) -- (Team 1 Fields, Team 1 Names)
+    -> (Element,[String]) -- (Team 2 Fields, Team 2 Names)
+    -> Element -- Score Graph
+    -> Element -- Scenario Preview
+    -> Element -- Chatbox
+    -> [String] -- Messages
+    -> Element
+draw gameStarted startGame scenarios team1 team2 scoreGraph scenarioPreview chatbox messages = 
+  if   gameStarted
+  then flow down 
+      [ scoreGraph
+      , spacer 20 1 `beside` chatbox
+      , spacer 20 1 `beside` flow down (map plainText messages)
+      ]
+  else 
+    spacer 20 1 `beside` 
+    (width 400 <| flow down
+      [ welcome
+      , flow down scenarios
+      , spacer 1 40
+      -- , plainText "Enter Team 1 Player Names"
+      , fst team1
+      , spacer 1 40
+      -- , plainText "Enter Team 2 Player Names"
+      , fst team2
+      , spacer 1 40
+      , startGame
+      ]) `beside`
+    spacer 40 1 `beside` 
+    scenarioPreview
+
+main = draw <~ 
+  gameStarted ~
+  constant startGame ~
   combine [scenario1,scenario2,scenario3] ~
-  scoreGraph ~
   team1Players ~
   team2Players ~
-  gameStarted
+  scoreGraph ~
+  (constant (flow down <| map (\_ -> plainText "Scenario Preview Goes Over Here") [1..40])) ~
+  msgBox ~
+  messages
